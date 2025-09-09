abb0cffc432bf1b3265fb1b8fce6749a1d49b880
Perry <aidanpj18@gmail.com>
2025-09-08 22:01:34 -0400

    Update Veld grammar revision and enable nested member access highlighting

--- languages/veld/highlights.scm
+++ languages/veld/highlights.scm
@@ -190,10 +190,10 @@
 ;   member: (identifier) @property)
 
 ; ; Member access in nested chains (object side)
-; (member_access
-;   object: (postfix_expression
-;     (member_access
-;       member: (identifier) @property)))
+(member_access
+  object: (postfix_expression
+    (member_access
+      member: (identifier) @property)))
 
 ; ; Function calls in chained member access
 ; (member_access
